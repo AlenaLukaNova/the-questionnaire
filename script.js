2e82fb361932375891b27592d525739d8cf635a4
Alena Lukyanava <99250387+AlenaLukaNova@users.noreply.github.com>
2023-11-05 12:12:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const form = document.querySelector(".form");
+const userName = document.querySelector('#name');
+
 form.addEventListener("submit", (event) => {
   // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
   // https://learn.javascript.ru/default-browser-action
